5ef2e9995cdfa448735f19b5b87692aca4ef1e4c
Jahongir-web <jahongirmh@gmail.com>
2022-05-21 05:21:27 +0500

    change create foodCtrl

--- controllers/foodCtrl.js
+++ controllers/foodCtrl.js
@@ -1,4 +1,13 @@
+const router = require("express").Router();
+const cloudinary = require("cloudinary");
+const fs = require("fs");
 const Food = require("../models/foodModel");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 const foodCtrl = {
   getFoods: async (req, res) => {
@@ -11,8 +20,38 @@ const foodCtrl = {
   },
   createFood: async (req, res) => {
     try {
+      
+      if (!req.files || Object.keys(req.files).length === 0)
+      return res.json('No upload!');
+      
+      const file = req.files.files;
+      if (file.size > 15 * 1024 * 1024) {
+        removeTmp(file.tempFilePath);
+        return res.error.invalidSize(res);
+      }
+
+      if (file.mimetype !== "image/jpeg" && file.mimetype !== "image/png") {
+        removeTmp(file.tempFilePath);
+        return res.error.invalidType(res);
+      }
+
+      await cloudinary.v2.uploader.upload(
+        file.tempFilePath,
+        { folder: "Food app" },
+        async (err, result) => {
+          if (err) throw err;
+
+          removeTmp(file.tempFilePath);
+
+          // res.json({ public_id: result.public_id, url: result.secure_url });
+          req.body.image = { public_id: result.public_id, url: result.secure_url }
+        }
+      );
+
       const { content, image, price } = req.body;
 
+      console.log(content, image, price);
+        
       const newFood = new Food({
         content,
         image,
@@ -44,6 +83,12 @@ const foodCtrl = {
       return res.error.handleError(res, err);
     }
   },
+};
+
+const removeTmp = (path) => {
+  fs.unlink(path, (err) => {
+    if (err) throw err;
+  });
 };
 
 module.exports = foodCtrl;
